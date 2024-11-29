1fc67738db04614dc24d8cafcf383e18c81f6431
loveleenn <157479716+loveleenn@users.noreply.github.com>
2024-11-29 20:40:48 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,54 @@
+/*var x = 52, y=10;
+x=x%y;
+x
+var a= 10, b=2;
+a=a**b;
+a*/
+
+
+/*var x= 3 =='3';
+x
+var y =2 ==='2';
+y
+var z =1 !=='1';
+z
+var w=5;
+--w;
+w*/
+
+/*
+
+         STRINGS.
+
+var favshow = "lord of the rings";
+console.log(favshow.length)
+console.log(favshow[3]);
+console.log(favshow[12]);
+console.log(favshow[favshow.length-1]);
+console.log(favshow.indexOf("of"));
+console.log(favshow.slice(5,7));
+var srk = "My name is lovy";
+console.log(favshow.toUpperCase());
+console.log(favshow.toLowerCase());
+*/
+
+/*
+        OBJECTS.
+
+let animal = {
+    name: "cow",
+    legs: 4
+}
+console.log(animal.name)
+
+let legsprop = "legs"
+console.log(animal[legsprop]);
+*/
+
+/* 
+  ARRAY
+let books = ["no.1", "No.2", "No.3"]
+console.log(books.length)
+*/
+
+
